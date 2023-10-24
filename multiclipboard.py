08357ab1eb3be7caad08e21abb24cfdcd71026e3
Aranya Dutta <98299268+thisizaro@users.noreply.github.com>
2023-10-24 18:44:20 +0530

    Add files via upload

---
+++ multiclipboard.py
@@ -0,0 +1,42 @@
+import sys
+import clipboard
+import json
+
+SAVED_DATA = "clipboard.json"
+
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)    
+
+    if command == 'save':
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+
+    elif command == "load":
+        print("load")
+
+    elif command == "list":
+        print("ls")
+
+    else:
+        print("Unknow command")
+
+else:
+    print("Please enter 1 command.")
+
+
+
